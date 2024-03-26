22d6f1827d2bd32b40d6ce4ca9189d26b2ed01e7
aaglis <aglissilvafon@gmail.com>
2024-03-25 23:16:09 -0300

    feat: template basico do componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, NewComponent],
+  template: `
+    <!-- <router-outlet /> -->
+    <h1>Curso de Angular 17!</h1>
+    <app-new-component />
+  `
 })
 export class AppComponent {
   title = 'projeto-latest-version';
